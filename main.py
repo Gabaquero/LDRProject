35669332449d723abd9038fa0fce1ab6e388257c
Gabaquero <95002161+Gabaquero@users.noreply.github.com>
2022-02-17 15:31:59 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -2,31 +2,66 @@ import cv2
 import numpy as np
 import utlis
 
+curveList = []
+avgVal = 10
 
-def getLaneCurve(img):
+
+def getLaneCurve(img, display=2):
     imgCopy = img.copy()
-    ##1
+    imgResult = img.copy()
+    # 1
     imgThres = utlis.thresholding(img)
-    ##2
-    h, w, c = img.shape
+    # 2
+    hT, wT, c = img.shape
     points = utlis.valTrackbars()
-    imgWarp = utlis.WarpImg(imgThres, points, w, h)
+    imgWarp = utlis.WarpImg(imgThres, points, wT, hT)
     imgWarpPoints = utlis.drawPoints(imgCopy, points)
-    ##3
-    basePoint, imgHist = utlis.getHistogram(imgWarp, display=True)
+    # 3
+    midPoint, imgHist = utlis.getHistogram(imgWarp, display=True, minPer=0.5, region=4)
+    curveAveragePoint, imgHist = utlis.getHistogram(imgWarp, display=True, minPer=0.9)
+    curveRaw = curveAveragePoint - midPoint
+    # 4
+    curveList.append(curveRaw)
+    if len(curveList) > avgVal:
+        curveList.pop(0)
+    curve = int(sum(curveList)/len(curveList))
+    #5
+    if display != 0:
+        imgInvWarp = utlis.WarpImg(imgWarp, points, wT, hT, inv=True)
+        imgInvWarp = cv2.cvtColor(imgInvWarp, cv2.COLOR_GRAY2BGR)
+        imgInvWarp[0:hT // 3, 0:wT] = 0, 0, 0
+        imgLaneColor = np.zeros_like(img)
+        imgLaneColor[:] = 0, 255, 0
+        imgLaneColor = cv2.bitwise_and(imgInvWarp, imgLaneColor)
+        imgResult = cv2.addWeighted(imgResult, 1, imgLaneColor, 1, 0)
+        midY = 450
+        cv2.putText(imgResult, str(curve), (wT // 2 - 80, 85), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 0, 255), 3)
+        cv2.line(imgResult, (wT // 2, midY), (wT // 2 + (curve * 3), midY), (255, 0, 255), 5)
+        cv2.line(imgResult, ((wT // 2 + (curve * 3)), midY - 25), (wT // 2 + (curve * 3), midY + 25), (0, 255, 0), 5)
+        for x in range(-30, 30):
+            w = wT // 20
+            cv2.line(imgResult, (w * x + int(curve // 50), midY - 10),
+                     (w * x + int(curve // 50), midY + 10), (0, 0, 255), 2)
+        #fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+        #cv2.putText(imgResult, 'FPS ' + str(int(fps)), (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (230, 50, 50), 3);
+    if display == 2:
+        imgStacked = utlis.stackImages(0.7, ([img, imgWarpPoints, imgWarp],
+                                             [imgHist, imgLaneColor, imgResult]))
+        cv2.imshow('ImageStack', imgStacked)
+    elif display == 1:
+        cv2.imshow('Resutlt', imgResult)
 
-    cv2.imshow('Thres', imgThres)
-    cv2.imshow('Warp', imgWarp)
-    cv2.imshow('Warp Points', imgWarpPoints)
-    cv2.imshow('Histogram', imgHist)
-    return None
+
+
+    return curve
 
 
 if __name__ == '__main__':
-    cap = cv2.VideoCapture('Images/vid1.mp4')
-    initialTrackBarVals = [105, 80, 20, 214]
+    cap = cv2.VideoCapture(0)
+    initialTrackBarVals = [0, 240, 0, 0]
     utlis.initializeTrackbars(initialTrackBarVals)
     frameCounter = 0
+
     while True:
         frameCounter += 1
         if cap.get(cv2.CAP_PROP_FRAME_COUNT) == frameCounter:
--- utlis.py
+++ utlis.py
@@ -2,60 +2,103 @@ import cv2
 import numpy as np
 
 
-
 def thresholding(img):
-    imgHsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    lowerWhite = np.array([80, 0, 0])
-    UpperWhite = np.array([255, 160, 255])
+    imgHsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    lowerWhite = np.array([25, 30, 25])
+    UpperWhite = np.array([125, 75, 255])
     maskWhite = cv2.inRange(imgHsv, lowerWhite, UpperWhite)
     return maskWhite
 
-def WarpImg(img, points,w,h):
+
+def WarpImg(img, points, w, h, inv=False):
     pts1 = np.float32(points)
     pts2 = np.float32([[0, 0], [w, 0], [0, h], [w, h]])
+    if inv:
+        matrix = cv2.getPerspectiveTransform(pts1, pts2)
+    else:
+        matrix = cv2.getPerspectiveTransform(pts1, pts2)
     matrix = cv2.getPerspectiveTransform(pts1, pts2)
     imgWarp = cv2.warpPerspective(img, matrix, (w, h))
     return imgWarp
 
+
 def nothing(a):
     pass
 
-def initializeTrackbars(intialTracbarVals,wT=480, hT=240):
+
+def initializeTrackbars(intialTracbarVals, wT=480, hT=240):
     cv2.namedWindow("Trackbars")
     cv2.resizeWindow("Trackbars", 360, 240)
-    cv2.createTrackbar("Width Top", "Trackbars", intialTracbarVals[0],wT//2, nothing)
+    cv2.createTrackbar("Width Top", "Trackbars", intialTracbarVals[0], wT // 2, nothing)
     cv2.createTrackbar("Height Top", "Trackbars", intialTracbarVals[1], hT, nothing)
-    cv2.createTrackbar("Width Bottom", "Trackbars", intialTracbarVals[2],wT//2, nothing)
+    cv2.createTrackbar("Width Bottom", "Trackbars", intialTracbarVals[2], wT // 2, nothing)
     cv2.createTrackbar("Height Bottom", "Trackbars", intialTracbarVals[3], hT, nothing)
+
 
 def valTrackbars(wT=480, hT=240):
     widthTop = cv2.getTrackbarPos("Width Top", "Trackbars")
     heightTop = cv2.getTrackbarPos("Height Top", "Trackbars")
     widthBottom = cv2.getTrackbarPos("Width Bottom", "Trackbars")
     heightBottom = cv2.getTrackbarPos("Height Bottom", "Trackbars")
-    points = np.float32([(widthTop, heightTop), (wT-widthTop, heightTop),
-                      (widthBottom, heightBottom), (wT-widthBottom, heightBottom)])
+    points = np.float32([(widthTop, heightTop), (wT - widthTop, heightTop),
+                         (widthBottom, heightBottom), (wT - widthBottom, heightBottom)])
     return points
+
 
 def drawPoints(img, points):
     for x in range(4):
         cv2.circle(img, (int(points[x][0]), int(points[x][1])), 15, (0, 0, 255), cv2.FILLED)
     return img
 
-def getHistogram(img, display=False, minPer=0.1):
+
+def getHistogram(img, display=False, minPer=0.1, region=1):
+    if region == 1:
+        histvalues = np.sum(img, axis=0)
+    else:
+        histvalues = np.sum(img[img.shape[0] // region::], axis=0)
     histValues = np.sum(img, axis=0)
     maxValue = np.max(histValues)
-    minVal = minPer*maxValue
-    indexArray = np.where(histValues >= minVal) #indexes with min values or above
-    basePoint = int(np.average(indexArray))#average of all max index values
+    minVal = minPer * maxValue
+    indexArray = np.where(histValues >= minVal)  # indexes with min values or above
+    basePoint = int(np.average(indexArray))  # average of all max index values
     print(basePoint)
 
     if display:
         imgHist = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
         for x, intensity in enumerate(histValues):
-            cv2.line(imgHist, (x, img.shape[0]), (x, img.shape[0]-intensity//255), (255, 0, 255), 1)
+            cv2.line(imgHist, (x, img.shape[0]), (x, img.shape[0] - intensity // 255 // region), (255, 0, 255), 1)
             cv2.circle(imgHist, (basePoint, img.shape[0]), 20, (0, 255, 255), cv2.FILLED)
         return basePoint, imgHist
 
     return basePoint
 
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
